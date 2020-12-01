8fb78888fb1c612c91314b0ed95a8f54a3e8061b
manitsoni <40520393+manitsoni@users.noreply.github.com>
2020-12-01 17:58:12 +0530

    Add files via upload

---
+++ SourceControlAssignment1/SourceControlAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceControlAssignment1.Models;
+namespace SourceControlAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(UserRegister objUr)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Create");
+            }
+            else
+            {
+                ViewBag.message = "Successfully Inserting Data.....!";
+                return View("Create");
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
